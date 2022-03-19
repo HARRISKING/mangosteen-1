6677515bb45aef16e7f38a03d960be249ff3c18f
suixing <suixing@uni-ubi.com>
2022-03-19 14:23:46 +0800

    feat: create \show 功能完成

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,16 +1,21 @@
 class UsersController < ApplicationController
   def create
-    p '你访问了create'
-    user = User.new email: 'llc@qq.com', name: 'harrisking'
+    user = User.new email: '123@qq.com', name: 'harrisking'
     if user.save
-      p '保存数据成功'
       render json: user
     else
-      p '报错'
+      render json: user.errors
     end
   end
 
   def show
     p '你访问了show'
+    user = User.find_by_id params[:id]
+    render json: user
+    if user
+      render json: user.errors
+    else
+      head 404 
+    end
   end
 end
