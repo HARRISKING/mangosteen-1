2f0c483c74fffc0f03d24890f62c0145f8e797c6
suixing <suixing@uni-ubi.com>
2022-03-28 20:08:59 +0800

    feat: 分页添加

--- app/controllers/api/v1/items_controller.rb
+++ app/controllers/api/v1/items_controller.rb
@@ -1,17 +1,26 @@
 class Api::V1::ItemsController < ApplicationController
- def index
 
+
+ def index
+  items = Item.page params[:page]
+
+  render json: {
+   resource: items,
+   pager: {
+    index: params[:page],
+    length: 10,
+    count: Item.count
+   }
+  }
  end
 
  def create 
   item = Item.new amount: 1
- if item.save
-  render json: {resource: item}
- else
-  render json: {errors: item.errors}
- end
-
-  
+  if item.save
+   render json: {resource: item}
+  else
+   render json: {errors: item.errors}
+  end
  end
 
 end
